9e618d24b192ad735834d6a9fb64e5fb60ee07f3
Haschm05 <haydensemail@sio.midco.net>
2025-02-12 09:12:11 -0600

    Main and account are now in their begginning stages

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Account account1 = new Account();
+        System.out.println(account1.getId());
+
+        Account account2 = new Account();
+        System.out.println(account2.getId());
+    }
+}
+
