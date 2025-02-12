18bd9971114804683ed17c1831dbc0dcd6128a67
Andrea <andreaolivares2006@gmail.com>
2025-02-12 09:23:45 -0600

    added the lastName, FirstName, and the double for balance

--- src/Account.java
+++ src/Account.java
@@ -2,11 +2,15 @@ public class Account {
     private static int lastId = 0;
 
     private final int id;
+    private String firstName;
+    private String lastName;
+    private double balance;
 
     public Account() {
         id = ++lastId;
     }
-     public int getId() {
+    public int getId() {
+
         return id;
-     }
+    }
 }
